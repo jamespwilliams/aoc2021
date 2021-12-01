a3e030f427b86e5d36fb4ecb44c7947219789df0
James Williams <jamesphwilliams@gmail.com>
2021-12-01 11:50:36 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+
+let
+  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
+in
+
+pkgs.mkShell {
+   buildInputs = with ocamlPackages; [
+    ocaml
+    findlib
+    dune_2
+    ocaml-lsp
+    ocamlformat
+    core
+  ];
+}
