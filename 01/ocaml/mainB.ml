a5570994fa3457499df1a6ab176461e3b825dead
James Williams <jamesphwilliams@gmail.com>
2021-12-01 08:41:47 +0000

    Complete day 1

---
+++ 01/ocaml/mainB.ml
@@ -0,0 +1,11 @@
+open Core
+
+let slurp file = In_channel.read_lines file
+
+let input = List.map ~f:int_of_string (slurp "../input.txt")
+
+let rec countIncreases list = match list with
+  | a::(b::c::d::_ as tail) -> (if ((a + b + c) < (b + c + d)) then 1 else 0) + countIncreases tail
+  | _                       -> 0
+
+let () = print_endline (string_of_int (countIncreases input))
---
+++ README.md
@@ -0,0 +1,4 @@
+# aoc2021
+
+Disclaimer: there's a 99% chance I'll give up on this after a few days, but
+anyway, here are some OCaml solutions.
